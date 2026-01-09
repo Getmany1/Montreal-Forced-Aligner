42f42d9bda642b042c8fd656415b850c5ee8b57b
Yaroslav Getman <yaroslav.getman@aalto.fi>
2026-01-09 11:54:55 +0200

    Change subset size 150000 -> 140000

--- montreal_forced_aligner/acoustic_modeling/trainer.py
+++ montreal_forced_aligner/acoustic_modeling/trainer.py
@@ -216,12 +216,12 @@ class TrainableAligner(TranscriberMixin, TopLevelMfaWorker, ModelExporterMixin):
         )
         training_params.append(("pronunciation_probabilities", {"subset": 50000}))
         training_params.append(
-            ("sat", {"subset": 120000, "num_leaves": 5000, "max_gaussians": 100000})
+            ("sat", {"subset": 140000, "num_leaves": 5000, "max_gaussians": 100000})
         )
         training_params.append(
             (
                 "pronunciation_probabilities",
-                {"subset": 120000, "optional": True},
+                {"subset": 140000, "optional": True},
             )
         )
         training_params.append(
